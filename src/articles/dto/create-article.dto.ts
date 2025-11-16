e27dbf2514a9173ddda8bdf2f4683996eba4f725
Dmitry Mezhinsky <mezhinsky.dmitry@gmail.com>
2025-11-16 11:14:05 +0300

    feat: enhance CreateArticleDto with validation decorators for title, description, content, and published fields

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,15 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsJSON,
+  IsObject,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class CreateArticleDto {
   @ApiProperty()
+  @IsString()
   title: string;
 
   @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
   description?: string;
 
   @ApiProperty()
+  @IsObject()
   content: string;
 
   @ApiProperty({ required: false, default: false })
+  @IsBoolean()
+  @IsOptional()
   published?: boolean = false;
 }
